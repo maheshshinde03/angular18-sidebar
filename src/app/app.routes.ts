f3607f83e623baa4adda0dc154fbabf2f1ee583a
Mahesh Shinde <maheshshinde028@gmail.com>
2024-10-06 17:32:58 +0530

    only sidebar code in angular 18

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,53 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { SubMenuLayoutComponent } from './components/sub-menu-layout/sub-menu-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    // {
+    //     path: '',
+    //     component: LayoutComponent,
+    //     children:[
+    //         {
+    //             path:'employee',
+    //             component: EmployeeComponent,
+    //             title: 'Employee'
+    //         },
+    //         {
+    //             path:'dashboard',
+    //             component: DashboardComponent,
+    //             title: 'Dashboard'   //you can set title here in angular 8 which will show on browser
+    //         }
+            
+    //     ]
+    // }
+
+    {
+        path: '',
+        component: SubMenuLayoutComponent,               //created new layout which has expandable menu
+        children:[
+            {
+                path:'dashboard',
+                component: DashboardComponent,
+                title: 'Dashboard'   //you can set title here in angular 8 which will show on browser
+            },
+            {
+                path:'employee',
+                component: EmployeeComponent,
+                title: 'Employee'
+            }
+            
+        ]
+    }
+];
